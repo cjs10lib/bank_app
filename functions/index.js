2d5f399f1b1332344f1359acc75f6afdc9d5e93e
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-12-29 21:00:27 +0000

    Implemented welcome-message function to push notification on sign-up

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+var msgData = 'Thank you for signing up. We will review your credentials and notify you on approval.'
+
+exports.welcomeMessage = functions.firestore
+    .document('account/{accountId}')
+    .onCreate((snapshot, context) => {
+        admin.firestore().collection('pushtokens').doc(snapshot.id).get().then((snapshots) => {
+            var token;
+            if (snapshots.exists) {
+                token = snapshots.data().token;
+            } else {
+                console.log('No device token found!');
+            }
+
+            var payload = {
+                "notification": {
+                    "title": "KAMCCU CORP CREDIT UNION",
+                    "body": msgData,
+                    "notification": "default"
+                },
+                "data": {
+                    "sendername": "KAMCCU CORP CREDIT UNION",
+                    "message": msgData
+                }
+            }
+
+            return admin.messaging().sendToDevice(token, payload).then((response) => {
+                console.log('Pushed message');
+            }).catch((error) => console.log(error));
+        })
+    })
